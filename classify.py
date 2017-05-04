0061ecba858ee7073ca0f03c52e3f069b1de9138
Erica Schwarz <eleighschwarz@gmail.com>
2017-05-04 11:44:08 -0400

    Merging files

--- classify.py
+++ classify.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import sys
 import pickle
+import numpy as np
 from Methods import ClassificationLabel, FeatureVector, Instance, Predictor, DecisionTree, NaiveBayes, NeuralNetwork
 
 def load_data(filename):
@@ -37,6 +38,8 @@ def get_args():
 	parser.add_argument("--mode", type=str, required=True, choices=["train", "test"], help="Mode: train or test.")
 	parser.add_argument("--model-file", type=str, required=True, help="Filename specifying where to save or load model.")
 	parser.add_argument("--algorithm", type=str, help="The name of the algorithm for training.")
+	parser.add_argument("--ig", type=str, choices = ["pure", "ratio"], help="Type of information gain.", default="pure")
+
 
 	args = parser.parse_args()
 	check_args(args)
@@ -44,19 +47,24 @@ def get_args():
 	return args
 
 def predict(predictor, instances):
+        y_true = []
+        y_pred = []
 	for instance in instances:
 		label = predictor.predict(instance)
 		print(str(label))
+                y_true.append(str(instance.getLabel()))
+                y_pred.append(str(label))
+        return y_true, y_pred
 
 def check_args(args):
 	if args.mode.lower() == "train":
 		if args.algorithm is None:
 			raise Exception("--algorithm must be specified in mode \"train\"")
 	else:
-		if not os.path.exists(args.model_file):
+		if not os.path.exists(args.model_file + "_" + args.algorithm + ".model"):
 			raise Exception("model file specified by --model-file does not exist.")
 
-def train(instances, algorithm):
+def train(instances, algorithm, ig):
 	"""
 	This is where you tell classify.py what algorithm to use for training
 	The actual code for training should be in the Predictor subclasses
@@ -69,14 +77,51 @@ def train(instances, algorithm):
                 predictor = NaiveBayes()
                 predictor.train(instances)
         elif algorithm.lower() == "decision_tree":
-                print "Training DecisionTree"
-                predictor = DecisionTree()
+                predictor = DecisionTree(ig)
                 predictor.train(instances)
         elif algorithm.lower() == "neural_network":
                 predictor = NeuralNetwork()
                 predictor.train(instances)
 
         return predictor
+
+def getStats(y_true, y_pred, filename):
+
+        
+        labels = list(set(y_true).union(set(y_pred)))
+
+        accuracy = 0.0
+        for i in xrange(len(y_true)):
+                if y_true[i] == y_pred[i]:
+                        accuracy += 1
+        accuracy = accuracy/len(y_true)
+
+        for label in labels:
+                pred_of_l = 0.0
+                true_of_l = 0.0
+                corr_of_l = 0.0
+                for i in xrange(len(y_true)):
+                        if y_true[i] == label:
+                                true_of_l += 1
+                        if y_pred[i] == label:
+                                pred_of_l += 1
+                        if y_pred[i] == label and y_true[i] == label:
+                                corr_of_l += 1
+                precision = corr_of_l/pred_of_l
+                recall = corr_of_l/true_of_l
+
+        try:
+                with open((str(filename)+"_stats.txt"), 'w') as writer:
+                        writer.write("Accuracy " + str(accuracy))
+                        writer.write('\n')
+                        writer.write("Precision " + str(precision))
+                        writer.write('\n')
+                        writer.write("Recall " + str(recall))
+                        writer.write('\n')
+
+        except IOError:
+                raise Exception("Exception while opening/writing file for writing predicted labels: " + predictions_file)
+
 
 def main():
 	args = get_args()
@@ -85,9 +130,9 @@ def main():
 		instances = load_data(args.data)
 
 		# Train
-		predictor = train(instances, args.algorithm)
+		predictor = train(instances, args.algorithm, args.ig)
 		try:
-			with open(args.model_file, 'wb') as writer:
+			with open((args.model_file + "_" + args.algorithm + ".model"), 'wb') as writer:
 				pickle.dump(predictor, writer)
 		except IOError:
 			raise Exception("Exception while writing to the model file.")
@@ -101,14 +146,18 @@ def main():
 		predictor = None
 		# Load model
 		try:
-			with open(args.model_file, 'rb') as reader:
+			with open((args.model_file + "_" + args.algorithm + ".model"), 'rb') as reader:
 				predictor = pickle.load(reader)
 		except IOError:
 			raise Exception("Exception while reading the model file.")
 		except pickle.PickleError:
 			raise Exception("Exception while loading pickle.")
 
-		predict(predictor, instances)
+		y_true, y_pred = predict(predictor, instances)
+                getStats(y_true, y_pred, args.algorithm)
+
+                
+                
 	else:
 		raise Exception("Unrecognized mode.")
 
